3ad64ab3216038885d6fe16c8cea7d3509430bb2
NanoMars <Armand.packham@gmail.com>
2024-08-28 21:36:49 +1000

    stuff displaying sorta, seperated everything into components for use

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-var canvas = document.createElement('canvas');
+var canvas = document.createElement('canvas')
 
-canvas.id = "CursorLayer";
-canvas.width = 1224;
-canvas.height = 768;
-canvas.style.zIndex = 8;
-canvas.style.position = "absolute";
-canvas.style.border = "1px solid";
+canvas.id = "CursorLayer"
+canvas.width = 1224
+canvas.height = 768
+canvas.style.zIndex = 8
+canvas.style.position = "absolute"
 
+var body = document.getElementsByTagName("body")[0]
+body.appendChild(canvas)
+cursorLayer = document.getElementById("CursorLayer")
+var ctx = canvas.getContext("2d")
 
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(canvas);
+function drawSVG(ctx, x, y, size, img, colour = undefined) {
+    console.log("1")
+    img.onload = function() {
+        console.log("2")
+        ctx.drawImage(img, x, y, img.width * size, img.height * size)
+        if (colour !== undefined) {
+            ctx.globalCompositeOperation = "source-in"
+            ctx.fillStyle = colour
+            ctx.fillRect(x, y, img.width * size, img.height * size)
+        }
+    }
+    console.log("3")
+}
 
-cursorLayer = document.getElementById("CursorLayer");
+var shirt = new Image()
+shirt.src = "./character/body/shirt.svg"
 
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
-ctx.fillRect(100, 100, 200, 200);
-ctx.fillStyle = "rgba(0, 255, 0, 0.2)";
-ctx.fillRect(150, 150, 200, 200);
-ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
-ctx.fillRect(200, 50, 200, 200);
+var head = new Image()
+head.src = "./character/body/head.svg"
+
+drawSVG(ctx, 0, 0, 5, head, "blue")
