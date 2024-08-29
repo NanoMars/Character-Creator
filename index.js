32bbf43c2b12a051911dfc16f8e69d2103a5f70f
NanoMars <Armand.packham@gmail.com>
2024-08-29 20:04:24 +1000

    drawing svgs using paths to support different colours and just to make my life easier, copy and pasted code from my personal website project to use another js script as a database so I can keep all of my designs in one file
    YIPPEEEEEE

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-var canvas = document.createElement('canvas')
+const canvas = document.createElement('canvas')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+document.body.appendChild(canvas)
+const ctx = canvas.getContext('2d')
 
-canvas.id = "CursorLayer"
-canvas.width = 1224
-canvas.height = 768
-canvas.style.zIndex = 8
-canvas.style.position = "absolute"
 
-var body = document.getElementsByTagName("body")[0]
-body.appendChild(canvas)
-cursorLayer = document.getElementById("CursorLayer")
-var ctx = canvas.getContext("2d")
+import {character} from "./db.js"
 
-function drawSVG(ctx, x, y, size, img, colour = undefined) {
-    console.log("1")
-    img.onload = function() {
-        console.log("2")
-        ctx.drawImage(img, x, y, img.width * size, img.height * size)
-        if (colour !== undefined) {
-            ctx.globalCompositeOperation = "source-in"
-            ctx.fillStyle = colour
-            ctx.fillRect(x, y, img.width * size, img.height * size)
+function searchByName(name) {
+    for (let i = 0; i < character.length; i++) {
+        if (character[i].name === name) {
+            return character[i].path
         }
     }
-    console.log("3")
+    return null
 }
 
-var shirt = new Image()
-shirt.src = "./character/body/shirt.svg"
+const shirt = searchByName('shirt')
 
-var head = new Image()
-head.src = "./character/body/head.svg"
+function drawPath(ctx, path, x, y, scale = 1, color = '#fffff') {
+    const path2D = new Path2D(path)
+    ctx.save()
+    ctx.translate(x, y)
+    ctx.scale(scale, scale)
+    ctx.fillStyle = color
+    ctx.fill(path2D)
+    ctx.restore()
+}
 
-drawSVG(ctx, 0, 0, 5, head, "blue")
+const pathPosition = { x: 100, y: 100, scale: 1, color: '#ed1c24' }
+
+drawPath(ctx, shirt, pathPosition.x, pathPosition.y, pathPosition.scale, pathPosition.color)
