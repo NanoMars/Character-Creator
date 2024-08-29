6f3ecf2e0e0921c5dfb13c9b359cfcbb3026bba7
NanoMars <Armand.packham@gmail.com>
2024-08-29 21:50:23 +1000

    lotta stuff, late, wanna go to sleep, check yourself

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// index.js
+
+
 const canvas = document.createElement('canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
@@ -6,7 +9,10 @@ const ctx = canvas.getContext('2d')
 
 import {body} from "./db.js"
 import {skinColours} from "./db.js"
-
+import {hairBack} from "./db.js"
+import {hairFront} from "./db.js"
+import {eyes} from "./db.js"
+import {hats} from "./db.js"
 
 function searchByName(name, data) {
     for (let i = 0; i < data.length; i++) {
@@ -16,10 +22,6 @@ function searchByName(name, data) {
     }
     return null;
 }
-
-const shirt = searchByName('shirt', body)
-const head = searchByName('head', body)
-const arms = searchByName('arms', body)
 
 function drawPath(ctx, path, x, y, scale = 1, color = '#fffff') {
     const path2D = new Path2D(path)
@@ -31,13 +33,26 @@ function drawPath(ctx, path, x, y, scale = 1, color = '#fffff') {
     ctx.restore()
 }
 
-function drawCharacter(x, y, scale = 1, skin) {
+const shirt = searchByName('shirt', body)
+const head = searchByName('head', body)
+const arms = searchByName('arms', body)
+const legs = searchByName('legs', body)
+
+function drawCharacter(x, y, scale = 1, skin, pantsColour, shirtColour, face, hat, hair) {
+    console.log(hair[2])
     const colour = skinColours[skin]
+    drawPath(ctx, hairBack[hair[0]], x, y, scale, hair[2])
     drawPath(ctx, head, x, y, scale, colour)
+    drawPath(ctx, hairFront[hair[1]], x, y, scale, hair[2])
+    drawPath(ctx, eyes[face], x + (75 * scale), y + (100 * scale), scale, "black")
     drawPath(ctx, arms, x + (90 * scale), y + (320 * scale), scale, colour)
-    drawPath(ctx, shirt, x + (85 * scale), y + (250 * scale), scale, '#ed1c24')
+    drawPath(ctx, legs, x + (133 * scale), y + (430 * scale), scale, pantsColour)
+    drawPath(ctx, shirt, x + (85 * scale), y + (250 * scale), scale, shirtColour)
+    const img = new Image();
+    img.src = hats[hat]
+    img.onload = function() {
+        ctx.drawImage(img, x, y - (img.height *  4/5 * scale) , img.width * 4 * scale, img.height * 4 * scale)
+    }
 }
 
-
-
-drawCharacter(10, 50, 2.2, 3)
+drawCharacter(1000, 2000, 2.2, 2, "pink", "lightgrey", 0, 0, [0, 0, '#aacede'])
