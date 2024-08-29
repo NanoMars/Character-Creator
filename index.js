1352d00e1c964de0dab37b3c31f6cfc7ea90e03e
NanoMars <Armand.packham@gmail.com>
2024-08-29 20:43:37 +1000

    programatically making people (evil laugh)

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ canvas.height = window.innerHeight
 document.body.appendChild(canvas)
 const ctx = canvas.getContext('2d')
 
+import {body} from "./db.js"
+import {skinColours} from "./db.js"
 
-import {character} from "./db.js"
 
-function searchByName(name) {
-    for (let i = 0; i < character.length; i++) {
-        if (character[i].name === name) {
-            return character[i].path
+function searchByName(name, data) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].name === name) {
+            return data[i].path
         }
     }
-    return null
+    return null;
 }
 
-const shirt = searchByName('shirt')
+const shirt = searchByName('shirt', body)
+const head = searchByName('head', body)
+const arms = searchByName('arms', body)
 
 function drawPath(ctx, path, x, y, scale = 1, color = '#fffff') {
     const path2D = new Path2D(path)
@@ -28,6 +31,13 @@ function drawPath(ctx, path, x, y, scale = 1, color = '#fffff') {
     ctx.restore()
 }
 
-const pathPosition = { x: 100, y: 100, scale: 1, color: '#ed1c24' }
+function drawCharacter(x, y, scale = 1, skin) {
+    const colour = skinColours[skin]
+    drawPath(ctx, head, x, y, scale, colour)
+    drawPath(ctx, arms, x + (90 * scale), y + (320 * scale), scale, colour)
+    drawPath(ctx, shirt, x + (85 * scale), y + (250 * scale), scale, '#ed1c24')
+}
 
-drawPath(ctx, shirt, pathPosition.x, pathPosition.y, pathPosition.scale, pathPosition.color)
+
+
+drawCharacter(10, 50, 2.2, 3)
